f37d4900a0cacdf7bd00da892bcd7f4c340bc58d
Bryan Channon <bryan@vuii.co.uk>
2016-01-18 14:26:46 +0000

    Update laracasts_download_helper.user.js

--- laracasts_download_helper.user.js
+++ laracasts_download_helper.user.js
@@ -6,12 +6,12 @@
 // @grant       none
 // ==/UserScript==
 
-$('.episode-wrap').each(function(index){
+$('.Lesson-List__item').each(function(index){
 
 	var id = $(this).find('input[name="lesson-id"]').val();
 	var title = $(this).find('.episode-title a').text().trim();
 
-	var dl = '<a href="/downloads/'+id+'?type=episode" title="'+ (index+1) + ' - ' +title+'" class="dl flaticon stroke btn-primary btn btn-xs icon-cloud"> <span>Download</span></a>';
+	var dl = '<a href="/downloads/'+id+'?type=episode" title="'+ (index+1) + ' - ' +title+'" class="Button Button--with-icon"> <span>Download</span></a>';
 
 	$(this).append( '<td>' + dl + '</td>' );
 
