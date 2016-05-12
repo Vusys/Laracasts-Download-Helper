eb2b2011800dbf5f15128ff777806be196b038d9
Bryan Channon <bryan@vuii.co.uk>
2016-05-12 23:12:10 +0100

    Update for site changes
    Old download links were changed in a site update, which broke this tool.
    
    Now it AJAX requests the video src to create download buttons. Should be more robust against site updates in the future.

--- laracasts_download_helper.user.js
+++ laracasts_download_helper.user.js
@@ -2,17 +2,33 @@
 // @name        Laracasts Download Helper
 // @namespace   Vusys
 // @include     https://laracasts.com/series/*
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
-$('.Lesson-List__item').each(function(index){
+$(document).ready(function () {
 
-	var id = $(this).find('input[name="lesson-id"]').val();
-	var title = $(this).find('.episode-title a').text().trim();
+	$('body').append('<a class="Button vdl" style="position:fixed;bottom:25px;left:25px;width:100px;">Get Download Buttons</a>');
 
-	var dl = '<a href="/downloads/'+id+'?type=episode" title="'+ (index+1) + ' - ' +title+'" class="Button Button--with-icon"> <span>Download</span></a>';
+	$(document).on('click', '.vdl', function () {
 
-	$(this).append( '<td>' + dl + '</td>' );
+		$(this).slideToggle();
+
+		$('.Lesson-List__item').each(function (index) {
+			
+			var id = $(this).find('input[name="lesson-id"]').val();
+			var title = $(this).find('.Lesson-List__title a').text().trim();
+			var url = $(this).find('.Lesson-List__title a').attr('href');
+			var that = $(this);
+
+			$.get(url, function (data) {
+				video_src = $(data).find('video source').attr('src');
+				var dl = '<a href="' + video_src + '" title="' + (index + 1) + ' - ' + title + '" target="_blank" class="Button Button--with-icon"> <span>Download</span></a>';
+				that.append('<span class="">' + dl + '</span>');
+			});
+
+		});
+
+	});
 
 });
