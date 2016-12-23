30230ad7420abf23d4a4ad060b53ca87966b4707
Bryan Channon <bryan@vuii.co.uk>
2016-12-23 11:42:17 +0000

    Update for site changes

--- laracasts_download_helper.user.js
+++ laracasts_download_helper.user.js
@@ -2,33 +2,32 @@
 // @name        Laracasts Download Helper
 // @namespace   Vusys
 // @include     https://laracasts.com/series/*
-// @version     2
+// @version     3
 // @grant       none
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
 $(document).ready(function () {
 
-	$('body').append('<a class="Button vdl" style="position:fixed;bottom:25px;left:25px;width:100px;">Get Download Buttons</a>');
+    $('body').append('<a class="button vdl" style="position:fixed;bottom:25px;left:25px;">Get Download Buttons</a>');
 
-	$(document).on('click', '.vdl', function () {
+    $(document).on('click', '.vdl', function () {
 
-		$(this).slideToggle();
+        $(this).slideToggle();
 
-		$('.Lesson-List__item').each(function (index) {
-			
-			var id = $(this).find('input[name="lesson-id"]').val();
-			var title = $(this).find('.Lesson-List__title a').text().trim();
-			var url = $(this).find('.Lesson-List__title a').attr('href');
-			var that = $(this);
+        $('.episode-list-item').each(function (index) {
+            var title = $(this).find('.episode-list-title a').text().trim();
+            var url = $(this).find('.episode-list-title a').attr('href');
+            var that = $(this);
 
-			$.get(url, function (data) {
-				video_src = $(data).find('video source').attr('src');
-				var dl = '<a href="' + video_src + '" title="' + (index + 1) + ' - ' + title + '" target="_blank" class="Button Button--with-icon"> <span>Download</span></a>';
-				that.append('<span class="">' + dl + '</span>');
-			});
+            $.get(url, function (data) {
+                video_src = $(data).find('video source').attr('src');
+                var dl = '<a href="' + video_src + '" title="' + (index + 1) + ' - ' + title + '" target="_blank" class="button"> <span>Download</span></a>';
+                that.append('<span class="">' + dl + '</span>');
+            });
 
-		});
+        });
 
-	});
+    });
 
 });
