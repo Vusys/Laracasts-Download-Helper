4b2496aad08976c932b46d4f288302f4913cd6f7
Bryan Channon <bryan@vuii.co.uk>
2023-05-22 15:38:20 +0100

    Add z-index to injected elements

--- laracasts_download_helper.user.js
+++ laracasts_download_helper.user.js
@@ -2,16 +2,15 @@
 // @name        Laracasts Download Helper
 // @namespace   Vusys
 // @include     https://laracasts.com/series/*
-// @version     7
+// @version     8
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
 jQuery(document).ready(function ($) {
+    $('body').append('<a class="vdl" style="position:fixed;bottom:25px;left:25px;background:black;color:white;padding:5px;z-index:999999;">Get yt-dlp commands</a>');
 
-    $('body').append('<a class="vdl" style="position:fixed;bottom:25px;left:25px;background:black;color:white;padding:5px;">Get yt-dlp commands</a>');
-
-    $('body').append('<span class="vdl-links" style="position:fixed;top:50px;left:25px;background:black;color:white;padding:5px 10px;max-width: 99%;overflow: scroll;font-family: monospace;font-size:10px;"></span>');
+    $('body').append('<span class="vdl-links" style="position:fixed;top:50px;left:25px;background:black;color:white;padding:5px 10px;max-width: 99%;overflow: scroll;font-family:monospace;font-size:10px;z-index:999999;"></span>');
 
     $(document).on('click', '.vdl', function () {
         $(this).slideToggle();
